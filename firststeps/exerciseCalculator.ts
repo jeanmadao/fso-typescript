c59e27a27210e3aac880f80a822ec30e7f03c15d
Jean Huynh <huynhjeanpierre@hotmail.com>
2023-09-09 16:09:05 +0200

    9.2 Exercise calculator

---
+++ firststeps/exerciseCalculator.ts
@@ -0,0 +1,44 @@
+interface exerciseResults {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+const calculateExercises = (
+  exerciseHours: number[],
+  target: number
+): exerciseResults => {
+  const periodLength = exerciseHours.length;
+  const trainingDays = exerciseHours.filter((hour) => hour > 0).length;
+  const average =
+    exerciseHours.reduce((acc, curr) => acc + curr, 0) / periodLength;
+  const rating = Math.max(Math.min(Math.round((2 * average) / target), 3), 1);
+  let ratingDescription: string;
+  switch (rating) {
+    case 1:
+      ratingDescription = "insufficient, you need to put more effort";
+      break;
+    case 2:
+      ratingDescription = "not too bad but could be better";
+      break;
+    case 3:
+      ratingDescription = "brilliant, keep it going";
+      break;
+  }
+  const success = average >= target;
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
