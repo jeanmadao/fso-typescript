fbcf53f90020146d428604be243b4e95279fe16b
Jean Huynh <huynhjeanpierre@hotmail.com>
2023-10-12 17:56:15 +0200

    9.22: Patientor, step3

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,8 +1,8 @@
-export type Diagnosis = {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
-};
+}
 
 export enum Gender {
   Male = "male",
@@ -10,7 +10,53 @@ export enum Gender {
   Other = "other",
 }
 
-export interface Entry {}
+type Date = string;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: Date;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: Date;
+  criteria: string;
+}
+
+interface HostpitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: Date;
+  endDate: Date;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HostpitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
